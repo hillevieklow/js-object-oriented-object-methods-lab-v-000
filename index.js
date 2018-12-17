253b60192b12cc84b63048276bcc27d1a11b367c
Hillevi Eklöw <hillevieklow@gmail.com>
2018-12-17 12:44:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ function BoardMember(name, homeState, training){
   this.veto = function(){
     console.log('No, I must disagree')
   }
+  this.approve = function(){
+    console.log('No, I must disagree')
+  }
+
 
 }
